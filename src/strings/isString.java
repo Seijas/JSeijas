4a211b5ea1e0000c06dd9d67e9eed60ed6a84f06
Seijas <srseijas94@gmail.com>
2015-10-17 21:22:47 +0200

    Add isCapitalStarted: check  if the first letter of string is Upper isLowerStarted: check  if the first letter of string is Lower

--- src/strings/isString.java
+++ src/strings/isString.java
@@ -2,8 +2,8 @@
 package strings;
 
 /**
- *
- * @author trusty
+ * @version 1.0
+ * @author Seijas
  */
 public class isString {
     
@@ -16,5 +16,21 @@ public class isString {
         return string.length() == 0;
     }
     
+    /**
+     * Check if the first letter of String is UpperCase
+     * @param string
+     * @return True: if is a UpperCase \nFalse if is a LowerCase
+     */
+    public static boolean isCapitalStarted(String string) {
+        return Character.isUpperCase(string.charAt(0));
+    }
     
+    /**
+     * Check if the first letter of String is LowerCase
+     * @param string
+     * @return True: if is a LowerCase \nFalse if is a UpperCase
+     */
+    public static boolean isLowerStarted(String string) {
+        return Character.isLowerCase(string.charAt(0));
+    }
 }
